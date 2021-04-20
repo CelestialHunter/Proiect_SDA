2b951e09ea80b62d22e8d1be4335695601c8c341
Andrei - Petru Balea <andrei.balea@student.upt.ro>
2021-04-20 17:49:11 +0300

    Schiță + definiție liste

--- Proiect_SDA/Proiect_SDA/main.c
+++ Proiect_SDA/Proiect_SDA/main.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 
+typedef struct {
+	char nume[20]; //20 de caractere maxim
+	char cod[4]; //4 numere - eventual, validare
+	enum {A=1, B=2, M=3} categorie;
+	char um[2]; //2 caractere - unitate de masura (ex: kg)
+	double cant; //cantitate - numar real
+	double pret; //pretul per unitate - numar real
+	// nu ma intereseaza aici magazia in care se situeaza, intrucat materialul se afla in lista unei magazii
+} Material;
+
+typedef struct {
+	Material* lista_materiale;
+} Magazie;
+
+Magazie A, B, M; //cele trei magazii
+
+
+
 int main() {
 
 }
